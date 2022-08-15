d4c63b344104a9c5c4abecf4733a4e054fc28c67
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-08-15 20:17:43 +0100

    Fix typo

--- src/tests/CardList.test.js
+++ src/tests/CardList.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import store from '../Redux/store';
+import store from '../redux/store';
 import CardList from '../components/CardList';
 
 it(' CardList Test "renders correctly"', () => {
